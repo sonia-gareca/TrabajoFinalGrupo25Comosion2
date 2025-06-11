51c6961b8703c81a205b72fc6eb75692f34b4a21
antonioAlmada <tonyramone1993@gmail.com>
2025-06-11 19:47:56 -0300

    Creacion del componente Navbar by antonioAlmada

--- src/assets/componentes/Navbar.jsx
+++ src/assets/componentes/Navbar.jsx
@@ -1,1 +1,23 @@
-//aqui va el menu de navegacion
+//Creamos el menu de navegacion
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Tienda El Hogar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="menu-principal" />
+        <Navbar.Collapse id="menu-principal">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/favoritos">Favoritos</Nav.Link>
+            <Nav.Link as={Link} to="/crear">Crear Producto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Menu;
